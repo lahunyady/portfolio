8032a34bd9ee82b9b5ae8c366b0a5bdcf36b8665
Laci <hu.laszlo93@gmail.com>
2020-11-15 11:41:11 +0100

    Finished Tutorial

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
@@ -12,7 +17,11 @@ function App() {
       <Navbar/>
       <Switch>
         <Route path='/' exact component={Home}/>
+        <Route path='/services' component={Services}/>
+        <Route path='/products' component={Products}/>
+        <Route path='/sign-up' component={SignUp}/>
       </Switch>
+      <Footer/>  
     </Router>
     </>
   );
