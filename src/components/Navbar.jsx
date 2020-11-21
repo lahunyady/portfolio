84e513057bc40809513d10a8ec281451abb7a4ba
ehunlas <laszlo.istvan.hunyady@ericsson.com>
2020-11-21 18:51:44 +0100

    personalyzing data

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
          <nav className="navbar">
              <div className="navbar-container">
                  <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                     <i className="fab fa-typo3"></i>
+                     <i className="fas fa-home"></i>
                  </Link>
                  <div className="menu-icon" onClick={handleClick}>
                      <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
