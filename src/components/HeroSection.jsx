ddfc5b6ba8a235e95011c5ea3097c5b909616260
ehunlas <laszlo.istvan.hunyady@ericsson.com>
2020-12-04 09:37:18 +0100

    Added button on home page
    - Fixed link colour in about me section
    - Added button on home page
    - Removed unused assets
    - Fixed visual bug in navbar

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './HeroSection.css';
 import '../App.css';
 import { useTranslation } from "react-i18next";
+import { Button } from '@material-ui/core';
 
 function HeroSection() {
     const { t } = useTranslation();
@@ -10,6 +11,9 @@ function HeroSection() {
         <div className='hero-container'>
             <h1>{t("Name")}</h1>
             <p>{t("Title")}</p>
+                <Button variant="contained"  href="/about-me">
+                    {t("About Me")}
+                </Button>
         </div>
     )
 }
